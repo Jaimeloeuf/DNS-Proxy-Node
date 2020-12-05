383f5c1738eb9bfd164692f32418c59160f32bdb
Jaime <jaimeloeuf@gmail.com>
2020-12-05 16:50:45 +0800

    Misc cleanup.

--- src/index.js
+++ src/index.js
@@ -31,19 +31,15 @@ const entries = [
 ];
 
 function proxy(question, response, cb) {
-  console.log("proxying", question.name);
-
-  var request = dns.Request({
+  const request = dns.Request({
     question: question, // forwarding the question
-    server: authority, // this is the authoritative DNS server requests are forwarded to
+    server: authority, // this is the authoritative DNS server where requests are forwarded to
     timeout: 700,
   });
 
   // when we get answers, append them to the response
-  request.on("message", (err, msg) => {
-    msg.answer.forEach((a) => response.answer.push(a));
-  });
-
+  // @todo Handle error(s)
+  request.on("message", (err, msg) => msg.answer.forEach(response.answer.push));
   request.on("end", cb);
   request.send();
 }
@@ -72,8 +68,6 @@ server.on("request", function handleRequest(request, response) {
 
         // Take care of CNAME requests too
         if (record.type == "CNAME") {
-          console.log("canme found");
-
           record.data = record.address;
           f.push((cb) =>
             proxy(
